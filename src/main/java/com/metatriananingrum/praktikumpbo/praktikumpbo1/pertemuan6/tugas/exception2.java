e39179939151b4cb89c7293992e450644718360f
meta triana <HP@LAPTOP-LKSOEVFL>
2022-12-13 11:41:38 +0700

    Pusing ria setelah pertemuan 8

--- src/main/java/com/metatriananingrum/praktikumpbo/praktikumpbo1/pertemuan6/tugas/exception2.java
+++ src/main/java/com/metatriananingrum/praktikumpbo/praktikumpbo1/pertemuan6/tugas/exception2.java
@@ -7,9 +7,6 @@ public class exception2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Masukan Angka : ");
-        String strInput = 5;
-        intInput = Integer.parseInt(strInput);
-        System.out.println(strInput);
         System.out.println("Berhasil");
         System.out.println("Tisak bisa memproses karna bukan angka");
     }
